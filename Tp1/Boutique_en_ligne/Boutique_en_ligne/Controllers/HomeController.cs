fa44f7417d9a1c3421df2ee8ecab74252b08cae0
pierl <pierl@PPUD.local>
2024-02-07 13:30:23 -0500

    Add Libman and nuget. Create Home index.html

---
+++ Tp1/Boutique_en_ligne/Boutique_en_ligne/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Boutique_en_ligne.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
